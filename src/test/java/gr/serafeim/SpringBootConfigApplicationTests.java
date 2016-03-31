bf5bb30244b42a92fcb56340493bfac89d33c03b
Serafeim Papastefanos <spapas@gmail.com>
2016-03-31 17:14:51 +0300

    Change yml to props and add test profiles

--- src/test/java/gr/serafeim/SpringBootConfigApplicationTests.java
+++ src/test/java/gr/serafeim/SpringBootConfigApplicationTests.java
@@ -2,15 +2,28 @@ package gr.serafeim;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringBootConfigApplication.class)
+@ActiveProfiles("test")
 public class SpringBootConfigApplicationTests {
+
+    @Value("${config.value}")
+    private String value;
+    
+    @Value("${spring.profiles.active}")
+    private String profile;
 
 	@Test
 	public void contextLoads() {
+        assertThat(value, is("Hello test!"));
+        assertThat(profile, is("test"));
 	}
-
 }
